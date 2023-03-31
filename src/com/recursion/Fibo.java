bfcef04dcd85147110590945d0d205e009e8e751
abyAnand <abianand382@gmail.com>
2023-03-31 20:38:06 +0530

    new method for finding fibonacci with memoization

--- src/com/recursion/Fibo.java
+++ src/com/recursion/Fibo.java
@@ -1,5 +1,8 @@
 package com.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibo {
 
     public static void main(String[] args) {
@@ -28,12 +31,33 @@ public class Fibo {
         return binary(arr, target, m+1, e);
     }
 
-    static int fibo(int n){
+    static int fiboOptimized(int n){
         //base condition
         if(n<2){
             return n;
         }
-        return fibo(n-1)+fibo(n-2);
+        return fiboOptimized(n-1)+ fiboOptimized(n-2);
     }
+
+    static final Map<Integer, Long> map = new HashMap<>();
+
+    public static long fibonacci(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be non-negative");
+        }
+
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+
+        long fib = fibonacci(n-1) + fibonacci(n-2);
+        map.put(n, fib);
+        return fib;
+    }
+
 
 }
